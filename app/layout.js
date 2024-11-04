7b1be8998186c98891e2d9780710e2fb8d3556b2
Omphile Morwane <omphilemorwane@gmail.com>
2024-11-04 14:57:33 +0200

    updated favicon size

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export const metadata = {
   description: "Arejeng Recipe App is your ultimate culinary companion, offering an extensive collection of easy-to-follow recipes that cater to every taste and occasion.",
   icons: {
     icon: [
-      { rel: "icon", url: "/favicon-192x192.png", sizes: "192x192" },
+      { rel: "icon", url: "/favicon-48x48.png", sizes: "48x48" },
       { rel: "icon", url: "/favicon.svg", type: "image/svg+xml" },
       { rel: "shortcut icon", url: "/favicon.ico" },
     ],
