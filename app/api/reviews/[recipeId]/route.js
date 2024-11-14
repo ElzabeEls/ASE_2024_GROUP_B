eb827cf0c860f75a887b4790cd8efd5c72f83103
Ikanyeng <ikanyengadams2@gmail.com>
2024-11-14 13:41:32 +0200

    Added MongoDB client and query to retrieve reviews by recipe ID

--- app/api/reviews/[recipeId]/route.js
+++ app/api/reviews/[recipeId]/route.js
@@ -1,15 +1,19 @@
 import { NextResponse } from 'next/server';
-
+import clientPromise from '../../../../lib/mongodb';
 
 export async function GET(request, { params }) {
   try {
-
+    const client = await clientPromise;
+    const db = client.db('devdb');
+    const collection = db.collection('reviews');
 
     const { recipeId } = params;
 
     
     const reviews = await collection.find({ recipeId })
-      
+      .sort({ date: -1 })
+      .project({ username: 1, date: 1, rating: 1, review: 1 })
+      .toArray();
 
     return NextResponse.json({ success: true, data: reviews });
   } catch (error) {
