9b4e58681ef9dbe1d84843e99d75ccf249b60bc9
NeoPhetoane <phetoaneneo4@gmail.com>
2024-11-22 14:20:12 +0200

    Debounce for waiting added: to guarantee that the debounce occurs even when the lengths of the queries is not met.

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useCallback, useRef } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { fetchRecipes } from "../../lib/api"; // Adjust this import based on your project structure
+import { fetchRecipes } from "../../lib/api";
 
 /**
  * A search bar component that allows users to search for recipes by title and category.
@@ -27,7 +27,6 @@ const SearchBar = () => {
   const [searchTagsQuery, setTagsSearchQuery] = useState(""); // State for the category input
   const [searchStepsQuery, setStepsSearchQuery] = useState(""); // State for the category input
 
-
   /**
    * Initializes the search query state from the URL search parameters.
    * Runs once on component mount and when searchParams changes.
@@ -51,7 +50,14 @@ const SearchBar = () => {
     }
 
     try {
-      const data = await fetchRecipes(1, 5, query, searchCategoryQuery, searchTagsQuery, searchStepsQuery); // Get limited suggestions
+      const data = await fetchRecipes(
+        1,
+        5,
+        query,
+        searchCategoryQuery,
+        searchTagsQuery,
+        searchStepsQuery
+      ); // Get limited suggestions
       setSuggestions(data);
       setShowSuggestions(data.length > 0);
     } catch (error) {
@@ -103,8 +109,8 @@ const SearchBar = () => {
     clearTimeout(debounceTimeout.current);
     clearTimeout(longQueryTimeout.current);
 
-     // Short query debounce (1-3 characters)
-     if (value.trim().length > 0 && value.trim().length <= 3) {
+    // Short query debounce (1-3 characters)
+    if (value.trim().length > 0 && value.trim().length <= 3) {
       debounceTimeout.current = setTimeout(() => {
         handleSearch();
       }, 300);
@@ -117,8 +123,13 @@ const SearchBar = () => {
         handleSearch();
       }, 500); // Debounce long queries with a delay of 500ms
     }
-  }
-  
+
+    // New debounce for submitting any query after 500ms
+    clearTimeout(debounceTimeout.current); // Clear previous timeout
+    debounceTimeout.current = setTimeout(() => {
+      handleSearch(); // Ensure the query is submitted after 500ms
+    }, 500);
+  };
 
   // Handle selection of an auto-suggested title
   const handleSuggestionClick = (title) => {
