e601cfbf24c078ed1d003178b772e684c50a54ee
Ikanyeng <ikanyengadams2@gmail.com>
2024-11-26 16:44:20 +0200

    Updated confirmation deletion modal in RecipeReviews component

--- app/components/RecipeReviews.js
+++ app/components/RecipeReviews.js
@@ -267,7 +267,18 @@ const RecipeReviews = ({ recipeId }) => {
 
       {/* On-screen confirmation for deletion */}
       {confirmDelete && (
-     
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-10">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-1/3">
+            <div className="flex justify-between">
+              <button
+                onClick={() => confirmDeleteReview(false)}
+                className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
       )}
     </section>
   );
