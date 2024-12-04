13f46a882294e0623b6e504b5c080a37d83206f5
Elzabé Els <elzzels25@gmail.com>
2024-12-04 09:30:28 +0200

    going back to previous nextConfig to see if recipes load

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    loader: "akamai",
-    path: "/",
+      remotePatterns: [
+          {
+          protocol: "https",
+          hostname: "img.sndimg.com",
+          port: "",
+          pathname: "/**",
+          },
+      ],
   },
 };
 
