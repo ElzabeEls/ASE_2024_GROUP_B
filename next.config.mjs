4b54ef11c070fd5ef8026d58e5d6ed329aa8db0a
Elzabé Els <elzzels25@gmail.com>
2024-12-04 09:35:52 +0200

    allowing CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-      remotePatterns: [
-          {
-          protocol: "https",
-          hostname: "img.sndimg.com",
-          port: "",
-          pathname: "/**",
+  async headers() {
+    return [
+      {
+        // Apply CORS headers to all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Update "*" to specific origins if possible
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { 
+            key: "Access-Control-Allow-Headers", 
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" 
           },
-      ],
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.sndimg.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
 export default nextConfig;
-
-
